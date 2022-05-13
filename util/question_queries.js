4c492b9341fc55f5b2faa2ad8c6521881bf731f7
Irfan Ahmed <irfanahmed1802@gmail.com>
2022-05-13 00:09:50 -0400

    Added Files

--- util/question_queries.js
+++ util/question_queries.js
@@ -7,12 +7,20 @@ const question_queries = {
         return await sql_queries.insert_query(insert_in_question);
     },
     get_all_questions_answers: async () => {
-        const get_questions_answers = `select q.*, a.answer_text, a.votes_count, a.user_id as anwered_by from question q left outer join answer a on q.id = a.question_id`;
+        const get_questions_answers = `select question_user_join.id as id_question, question_user_join.*, answer_user_join.* from
+            (select q.*, u.first_name as question_posted_by_first_name, u.last_name as question_posted_by_last_name from question q, user u where q.user_id = u.id) as question_user_join left outer join
+            (select a.*, u.first_name as answer_posted_by_first_name, u.last_name as answer_posted_by_last_name from answer a, user u where a.user_id = u.id) as answer_user_join
+            on question_user_join.id = answer_user_join.question_id`;
+
         console.log(get_questions_answers);
         return await sql_queries.select_query(get_questions_answers);
     },
     get_user_questions_answers: async ({user_id}) => {
-        const get_user_questions_answers = `select * from question left join answer a on question.id = a.question_id where question.user_id = ${user_id}`;
+        const get_user_questions_answers = `select question_user_join.id as id_question, question_user_join.*, answer_user_join.* from
+            (select q.*, u.first_name as question_posted_by_first_name, u.last_name as question_posted_by_last_name from question q, user u where q.user_id = u.id) as question_user_join left outer join
+            (select a.*, u.first_name as answer_posted_by_first_name, u.last_name as answer_posted_by_last_name from answer a, user u where a.user_id = u.id) as answer_user_join
+            on question_user_join.id = answer_user_join.question_id where question_user_join.user_id = ${user_id} or answer_user_join.user_id = ${user_id}`;
+
         console.log(get_user_questions_answers);
         return await sql_queries.select_query(get_user_questions_answers);
     }
